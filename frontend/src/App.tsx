c9d33dde380dbc66f7f4723ba6d4f4d04f0edb23
sunyoung315 <rlatjsdud315@gmail.com>
2024-05-26 08:50:06 +0900

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  margin: auto;
+  max-width: 430px;
+  min-height: 100vh;
+  background-color: #fffefc;
+`;
+
+function App() {
+  return <AppWrapper></AppWrapper>;
+}
+
+export default App;
