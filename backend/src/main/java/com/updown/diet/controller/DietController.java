75402c48b6e814d74da4837268da21dd9fbac04c
권유경 <yukyung531@gmail.com>
2024-08-06 20:15:39 +0900

    modify diet get uri

--- backend/src/main/java/com/updown/diet/controller/DietController.java
+++ backend/src/main/java/com/updown/diet/controller/DietController.java
@@ -55,7 +55,7 @@ public class DietController {
      * @return 식단 리스트
      */
     @Transactional
-    @GetMapping("/{regDate}")
+    @GetMapping("/daily/{regDate}")
     public ResponseEntity<?> searchDayDiet(@AuthenticationPrincipal Member member, @PathVariable("regDate") LocalDate regDate){
         List<DietDayRes> dietList = dietService.searchDayDiet(member, regDate);
         return ResponseEntity.ok().body(dietList);
