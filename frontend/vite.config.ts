630294003634b6029990099b5d0aeccb4b7d95c9
sunyoung315 <rlatjsdud315@gmail.com>
2024-08-11 02:53:20 +0900

    fix: add start url of pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
         enabled: true,
       },
       injectRegister: 'auto',
-      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'UpDown', // 설치 배너에 표시되는 이름
         short_name: 'UpDown', // 아이콘 아래에 표시될 이름
@@ -24,7 +23,7 @@ export default defineConfig({
         background_color: '#FFFEFC', // 앱의 배경 색상
         lang: 'ko', // 앱의 기본 언어
         display: 'standalone', // 브라우저 UI 없이 표시
-        // start_url: '/',
+        start_url: '/main',
         icons: [
           {
             src: 'pwa-64x64.png',
@@ -52,6 +51,33 @@ export default defineConfig({
           },
         ],
       },
+      // workbox: {
+      //   // 캐시 전략 설정
+      //   runtimeCaching: [
+      //     {
+      //       urlPattern: /^https:\/\/updown\.run\/.*$/, // API 호출 URL 패턴
+      //       handler: 'NetworkFirst', // 네트워크 우선 전략
+      //       options: {
+      //         cacheName: 'api-cache',
+      //         expiration: {
+      //           maxEntries: 50, // 최대 캐시 항목 수
+      //           maxAgeSeconds: 24 * 60 * 60, // 최대 캐시 시간 (1일)
+      //         },
+      //       },
+      //     },
+      //     {
+      //       urlPattern: /\.(?:js|css|html|png|jpg|jpeg|svg)$/, // 정적 자원 패턴
+      //       handler: 'CacheFirst', // 캐시 우선 전략
+      //       options: {
+      //         cacheName: 'static-resources',
+      //         expiration: {
+      //           maxEntries: 100, // 최대 캐시 항목 수
+      //           maxAgeSeconds: 30 * 24 * 60 * 60, // 최대 캐시 시간 (30일)
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     }),
   ],
   server: {
